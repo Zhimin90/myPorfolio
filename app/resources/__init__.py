e25bce969176864ab157d6785237c59fcefee9c8
Zhimin90 <zhiminzou@gmail.com>
2020-03-29 17:47:39 -0500

    added airtable endpoint

--- app/resources/__init__.py
+++ app/resources/__init__.py
@@ -1,6 +1,9 @@
-from .example import ns as example_ns
+#from .example import ns as example_ns
+from .example import ns_airtable as ns_airtable
+
 from ..utils import PatchedApi
 
 api = PatchedApi()
 
-api.add_namespace(example_ns)
+#api.add_namespace(example_ns)
+api.add_namespace(ns_airtable)
--- app/resources/example.py
+++ app/resources/example.py
@@ -1,18 +1,38 @@
 from flask_restplus import Namespace, Resource, fields
+import requests
+import os
 
-ns = Namespace('example', description='Examples')
+#ns = Namespace('example', description='Examples')
+ns_airtable = Namespace('airtable', description='Airtable')
 
-success_model = ns.model('Success', {
+#success_model = ns.model('Success', {
+#    'message': fields.String
+#})
+
+airtable_model = ns_airtable.model('Success', {
     'message': fields.String
 })
 
+airtableAPIKey = os.environ['VUE_APP_AIRTABLEKEY']
 
-@ns.route('', endpoint='index')
+@ns_airtable.route('', endpoint='index')
 class IndexPage(Resource):
 
-    @ns.marshal_with(success_model)
+    #@ns.marshal_with(success_model)
+    #def get(self):
+    #    """
+    #    Example url
+    #    """
+    #    return {'message': 'Success'}
+
+    @ns_airtable.marshal_with(airtable_model)
     def get(self):
         """
         Example url
         """
-        return {'message': 'Success'}
+        resp = requests.get(
+            'https://api.airtable.com/v0/appcQDvLw3YiggMPP/Projects/?maxRecords=3&view=All%20projects', headers={'Authorization': 'Bearer '  + airtableAPIKey})
+        data = resp.json()
+        print(data)
+        return 'dummy'
+
