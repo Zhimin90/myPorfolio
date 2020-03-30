494937daf042e549e1c3ce0547ebf1194026a1d9
Zhimin90 <zhiminzou@gmail.com>
2020-03-29 19:28:39 -0500

    updated home route to /home

--- vue_my_portfolio/src/App.vue
+++ vue_my_portfolio/src/App.vue
@@ -3,7 +3,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <nav class="navbar" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
-        <router-link class="navbar-item" to="/"> Welcome </router-link>
+        <router-link class="navbar-item" to="/home"> Welcome </router-link>
 
         <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" :class="{ 'is-active': showNav }" @click="showNav = !showNav">
           <span aria-hidden="true"></span>
@@ -18,7 +18,7 @@
         </div>
 
         <div class="navbar-end">
-          <router-link to="/" class="navbar-item">
+          <router-link to="/home" class="navbar-item">
            Home
           </router-link>
           <router-link to="/about" class="navbar-item">
